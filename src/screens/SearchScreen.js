a4512074e8184106fd3eb7845d1d6f69930d2482
Nick Sherman <ogleffeckt2@yahoo.com>
2019-11-28 18:24:17 -0500

    clickable restaurants now show user images

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = ({ navigation }) => {
+const SearchScreen = () => {
 	const [ term, setTerm ] = useState('');
 	const [ searchApi, results, errorMessage ] = useResults();
 
@@ -20,9 +20,9 @@ const SearchScreen = ({ navigation }) => {
 			<SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<ScrollView>
-				<ResultsList results={filterResultsByPrice('$')} title="Cost Effective" navigation={navigation}/>
-				<ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" navigation={navigation}/>
-				<ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" navigation={navigation}/>
+				<ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+				<ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+				<ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
 			</ScrollView>
 		</>
 	);
